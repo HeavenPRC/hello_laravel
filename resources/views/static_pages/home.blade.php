abd08288297cc2719e4ff683ca7ec43e8c9b533a
heaven <1353772647@qq.com>
2018-01-12 05:00:53 +0000

    finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,17 @@
 @extends('layouts.default')
+
+
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+    	<h1>Hello Laravel</h1>
+    	<p class="lead">
+    		您现在看到的是<a href="/">Laravel的艰难学习历程</a>
+    	</p>
+    	<p>
+    		实践是学习的最好方法。
+    	</p>
+    	<p>
+    		<a  class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+    	</p>
+    </div>
 @stop
