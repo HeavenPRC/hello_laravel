2ab6679c94bed543380c176cd10c0e8c24d9f458
heaven <1353772647@qq.com>
2018-01-15 15:34:48 +0000

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
 
 class UsersController extends Controller
 {
@@ -10,4 +12,35 @@ class UsersController extends Controller
     {
     	return view('users.create');
     }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    /**
+     * 表单限制
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎,n\您将在这里开始一段新的旅程~');
+        /*重定向到用户个人信息 隐性路由模型绑定*/
+        return redirect()->route('users.show', [$user]);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,24 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
+
+/*以上资源路由等同于以下代码
+
+1.显示所有用户列表的页面
+Route::get('/users', 'UsersController@index')->name('users.index');
+2.显示用户个人信息的页面
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+3.创建用户的页面
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+4.创建用户
+Route::post('/users', 'UsersController@store')->name('users.store');
+5.编辑用户个人资料的页面
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+6.更新用户
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+7.删除用户
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
+*/
